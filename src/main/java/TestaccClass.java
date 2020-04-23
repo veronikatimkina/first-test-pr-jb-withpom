e0d266e915ea4df0607dd3ce16062063bfc99c0f
Timkina Veronika <vtimkina@marathonbet.ru>
2020-04-23 21:27:54 +0300

    Commit_msg_for_rebase

--- src/main/java/TestaccClass.java
+++ src/main/java/TestaccClass.java
@@ -1,6 +1,6 @@
 public class TestaccClass {
     public static void main(String[] args) {
         int number = 12345;
-        System.out.println("number = " + number);
+        System.out.println(number);
     }
 }
