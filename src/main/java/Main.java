88231559537ba24008b9004e2e5f93275b1ae15a
Timkina Veronika <timkina.veronika@gmail.com>
2020-06-08 13:08:10 +0300

    qqq

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,16 @@ import java.util.LinkedList;
 public class Main {
     public static void main(String[] args) {
 
+        String str1 = "Hello World";
+        String str2 = "My name is Earl";
+        String str3 = "I love Java";
 
+        LinkedList<String> earlBio = new LinkedList<>();
+        earlBio.add(str1);
+        earlBio.add(str2);
+        earlBio.add(str3);
+
+        System.out.println(earlBio);
 
     }
 }
