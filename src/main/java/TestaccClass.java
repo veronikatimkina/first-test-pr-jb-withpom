d4d036083d7363bbaaa2a1c0f9daed0cdb3e53dc
Veronika.Timkina <Veronika.Timkina@jetbrains.com>
2020-06-09 15:02:03 +0300

    edit TestaccClass in Branch10

--- src/main/java/TestaccClass.java
+++ src/main/java/TestaccClass.java
@@ -1,6 +1,6 @@
 public class TestaccClass {
     public static void main(String[] args) {
-        int number = 12345;
-        System.out.println(number);
+        int number = 9999;
+        System.out.println("number = " + number);
     }
 }
