060a02da036735375ee9f8840c19042d4db11414
Veronika.Timkina <Veronika.Timkina@jetbrains.com>
2020-06-09 14:13:08 +0300

    edit Newclass in master

--- src/main/java/Newclass.java
+++ src/main/java/Newclass.java
@@ -1,5 +1,8 @@
 public class Newclass {
     public static void main(String[] args){
-        System.out.println(" ");
+
+        System.out.println("vrvfeververv");
+        System.out.println("qweqwe");
+
     }
 }
