eb3ccfe49318bc070aa1948ad750762e75800d5e
Veronika.Timkina <Veronika.Timkina@jetbrains.com>
2020-06-23 19:27:08 +0300

    edit Test in PRbranch1

--- src/main/java/pack/Test.java
+++ src/main/java/pack/Test.java
@@ -1,4 +1,56 @@
 package pack;
 
+import org.w3c.dom.ls.LSOutput;
+
 public class Test {
+
+
+    public void foo(){
+        /*After initializing a pull request, you'll see a review page that shows a high-level overview of the changes
+        between your branch (the compare branch) and the repository's base branch. You can add a summary of the proposed
+        changes, review the changes made by commits, add labels, milestones, and assignees, and @mention individual
+        contributors or teams. For more information, see "Creating a pull request."
+        Once you've created a pull request, you can push commits from your topic branch to add them to your existing
+        pull request. These commits will appear in chronological order within your pull request and the changes will be
+        visible in the "Files changed" tab.
+        Other contributors can review your proposed changes, add review comments, contribute to the pull request
+        discussion, and even add commits to the pull request.
+        You can see information about the branch's current deployment status and past deployment activity on the
+        "Conversation" tab. For more information, see "Viewing deployment activity for a repository."
+        After you're happy with the proposed changes, you can merge the pull request. If you're working in a shared
+        repository model, you create a pull request and you, or someone else, will merge your changes from your feature
+        branch into the base branch you specify in your pull request. For more information, see "Merging a pull request."
+        If status checks are required for a repository, the required status checks must pass before you can merge your
+        branch into the protected branch. For more information, see "About required status checks."
+        You can link a pull request to an issue to show that a fix is in progress and to automatically close the issue
+        when someone merges the pull request. For more information, see "Linking a pull request to an issue."
+        You can visit your dashboard to quickly find links to recently updated pull requests you're working on or
+        subscribed to. For more information, see "About your personal dashboard."
+        Draft pull requests
+        Draft pull requests are available in public repositories with GitHub Free and GitHub Free for organizations,
+        GitHub Pro, and legacy per-repository billing plans, and in public and private repositories with GitHub Team
+        and GitHub Enterprise Cloud. For more information, see "GitHub's products."
+        When you create a pull request, you can choose to create a pull request that is ready for review or a draft pull
+        request. Draft pull requests cannot be merged, and code owners are not automatically requested to review draft
+        pull requests. For more information about creating a draft pull request, see "Creating a pull request" and
+        "Creating a pull request from a fork."
+        When you're ready to get feedback on your pull request, you can mark your draft pull request as ready for
+        review. Marking a pull request as ready for review will request reviews from any code owners. You can convert
+        a pull request to a draft at any time. For more information, see "Changing the stage of a pull request."
+        Further reading
+        "Pull request" in the GitHub glossary
+        "About branches"
+        "Commenting on a pull request"
+        "Merging a pull request"
+        "Closing a pull request"
+        "Deleting unused branches"
+        "About pull request merges"
+         */
+
+
+
+   }
+    public static void main(String[] args) {
+        System.out.println();
+    }
 }
