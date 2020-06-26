ff1091da233ca824960c151a6325ea391e2b71be
Veronika.Timkina <Veronika.Timkina@jetbrains.com>
2020-06-26 17:32:35 +0300

    edit Main in PRbranch4

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,18 +3,7 @@ import java.util.LinkedList;
 public class Main {
     public static void main(String[] args) {
 
-        String str1 = "";
-        String str2 = "My name is Earl";
-        String str3 = "I love Java";
-      
-        int u =0;
 
-        LinkedList<String> earlBio = new LinkedList<>();
-        earlBio.add(str1);
-        earlBio.add(str2);
-        earlBio.add(str3);
-
-        System.out.println(earlBio);
 
     }
 }
