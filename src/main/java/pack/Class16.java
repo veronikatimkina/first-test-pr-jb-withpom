7ff50659985b156443a8427a71fd047cc20d5ea7
Veronika.Timkina <Veronika.Timkina@jetbrains.com>
2020-07-07 12:42:48 +0300

    commit from master with conflict

--- src/main/java/pack/Class16.java
+++ src/main/java/pack/Class16.java
@@ -2,6 +2,6 @@ package pack;
 
 public class Class16 {
     public static void main(String[] args) {
-        System.out.println("ggggg");
+        System.out.println("gggbgergregregegg");
     }
 }
