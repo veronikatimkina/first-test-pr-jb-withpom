c3c38d835b7eb7e63d0e6d38bb49ff5f55ce04cb
Veronika.Timkina <Veronika.Timkina@jetbrains.com>
2020-08-21 14:08:14 +0300

    test

--- src/main/java/Class002.java
+++ src/main/java/Class002.java
@@ -2,4 +2,5 @@ public class Class002 {
     public static void main(String[] args) {
         System.out.println("If <branch> is specified, git rebase will perform an automatic git switch <branch> before doing anything else. Otherwise it remains on the current branch.\n");
                 }
+                public void foo(){};
 }
