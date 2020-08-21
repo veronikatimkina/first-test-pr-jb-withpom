a67be207949d4a7e17a5fea9a48cf37fd79e2588
Veronika.Timkina <Veronika.Timkina@jetbrains.com>
2020-08-21 13:10:37 +0300

    test

--- src/main/java/Class002.java
+++ src/main/java/Class002.java
@@ -1,7 +1,5 @@
 public class Class002 {
     public static void main(String[] args) {
-        System.out.println("If <branch> is specified, git rebase will perform an automatic git switch <branch> before doing anything else. Otherwise it remains on the current branch.\n" +
-                "\n" +
-                "If <upstream> is not specified, the upstream configured in branch.<name>.remote and branch.<name>.merge options will be used (see git-config[1] for details) and the --fork-point option is assumed. If you are currently not on any branch or if the current branch does not have a configured upstream, the rebase will abort.");
-    }
+        System.out.println("If <branch> is specified, git rebase will perform an automatic git switch <branch> before doing anything else. Otherwise it remains on the current branch.\n");
+                }
 }
